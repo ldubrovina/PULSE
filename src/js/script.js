56dbec6bdfbc5c1a06510e5e839b27f91305047f
ldubrovina <dubrovinalv24@gmail.com>
2023-05-16 22:45:14 +0300

    feat: add jq script for change text in modal

--- src/js/script.js
+++ src/js/script.js
@@ -42,8 +42,13 @@ $(document).ready(function () {
     $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
   });
 
-  $('.button_mini').on('click', function () {
-    $('.overlay, #order').fadeIn('slow');
+
+
+  $('.button_mini').each(function (i) {
+    $(this).on('click', function () {
+      $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+      $('.overlay, #order').fadeIn('slow');
+    });
   });
 
 });
