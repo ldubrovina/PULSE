5d266c3aeae35d2c863c5aeaf911c39d2f8f078e
ldubrovina <dubrovinalv24@gmail.com>
2023-05-17 23:15:58 +0300

    feat:  tests php mailer

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('.carousel__inner').slick({
         speed: 1200,
         adaptiveHeight: true,
@@ -14,16 +14,16 @@ $(document).ready(function(){
             }
         ]
     });
-    
-    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
         $(this)
-          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
-          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+            .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+            .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
     });
 
     function toggleSlide(item) {
-        $(item).each(function(i) {
-            $(this).on('click', function(e) {
+        $(item).each(function (i) {
+            $(this).on('click', function (e) {
                 e.preventDefault();
                 $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
                 $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
@@ -36,21 +36,21 @@ $(document).ready(function(){
 
     // Modal
 
-    $('[data-modal=consultation]').on('click', function() {
+    $('[data-modal=consultation]').on('click', function () {
         $('.overlay, #consultation').fadeIn('slow');
     });
-    $('.modal__close').on('click', function() {
+    $('.modal__close').on('click', function () {
         $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
     });
 
-    $('.button_mini').each(function(i) {
-        $(this).on('click', function() {
+    $('.button_mini').each(function (i) {
+        $(this).on('click', function () {
             $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
             $('.overlay, #order').fadeIn('slow');
         })
     });
 
-    function validateForms(form){
+    function validateForms(form) {
         $(form).validate({
             rules: {
                 name: {
@@ -67,11 +67,11 @@ $(document).ready(function(){
                 name: {
                     required: "Пожалуйста, введите свое имя",
                     minlength: jQuery.validator.format("Введите {0} символа!")
-                  },
+                },
                 phone: "Пожалуйста, введите свой номер телефона",
                 email: {
-                  required: "Пожалуйста, введите свою почту",
-                  email: "Неправильно введен адрес почты"
+                    required: "Пожалуйста, введите свою почту",
+                    email: "Неправильно введен адрес почты"
                 }
             }
         });
@@ -83,13 +83,13 @@ $(document).ready(function(){
 
     $('input[name=phone]').mask("+7 (999) 999-99-99");
 
-    $('form').submit(function(e) {
+    $('form').submit(function (e) {
         e.preventDefault();
         $.ajax({
             type: "POST",
             url: "mailer/smart.php",
             data: $(this).serialize()
-        }).done(function() {
+        }).done(function () {
             $(this).find("input").val("");
             $('#consultation, #order').fadeOut();
             $('.overlay, #thanks').fadeIn('slow');
@@ -101,7 +101,7 @@ $(document).ready(function(){
 
     // Smooth scroll and pageup
 
-    $(window).scroll(function() {
+    $(window).scroll(function () {
         if ($(this).scrollTop() > 1600) {
             $('.pageup').fadeIn();
         } else {
@@ -109,10 +109,9 @@ $(document).ready(function(){
         }
     });
 
-    $("a[href^='#']").click(function(){
+    $("a[href^='#']").click(function () {
         const _href = $(this).attr("href");
-        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
         return false;
     });
 });
-
